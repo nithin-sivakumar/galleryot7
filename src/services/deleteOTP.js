b65697ff48f7498055dd98ba1da8137d98fdd748
Nithin <nithinsgayathri@gmail.com>
2024-03-24 13:30:12 +0530

    Removed log spam

--- src/services/deleteOTP.js
+++ src/services/deleteOTP.js
@@ -4,7 +4,7 @@ import { OTP } from "../models/otp.model.js";
 cron.schedule("* * * * *", async () => {
   try {
     await OTP.deleteMany({ expiry: { $lt: new Date() } });
-    console.log("Expired OTP entries deleted successfully");
+    // console.log("Expired OTP entries deleted successfully");
   } catch (error) {
     console.error("Error deleting expired OTP entries:", error);
   }
